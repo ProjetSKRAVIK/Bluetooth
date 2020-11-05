221a6e358db53c6e7c21412587b9b5a0084148b1
Nostralis <73784748+Nostralis@users.noreply.github.com>
2020-11-05 11:46:09 +0100

    Update EmetteurNRF24.ino

--- EmetteurNRF24.ino
+++ EmetteurNRF24.ino
@@ -1,75 +1,55 @@
-/* 1 ch NRF 24 TRANSMITTER example.
-/* Tutorial link: http://electronoobs.com/eng_arduino_tut95.php
- * Code: http://electronoobs.com/eng_arduino_tut95_code1.php
- * Scheamtic: http://electronoobs.com/eng_arduino_tut95_sch1.php
- * Youtube Channel: http://www.youtube/c/electronoobs   
- * 
-  Module // Arduino UNO    
-    GND    ->   GND
-    Vcc    ->   3.3V
-    CE     ->   D9
-    CSN    ->   D10
-    CLK    ->   D13
-    MOSI   ->   D11
-    MISO   ->   D12  
- */
-
-/* First we include the libraries. Download it from 
-   my webpage if you donw have the NRF24 library */ 
 #include <SPI.h>
-#include <nRF24L01.h>             //Downlaod it here: https://www.electronoobs.com/eng_arduino_NRF24.php
-#include <RF24.h>            
-/*//////////////////////////////////////////////////////*/
 
-/*Create a unique pipe out. The receiver has to 
-  wear the same unique code*/  
-const uint64_t pipeOut = 0xE8E8F0F0E1LL; //IMPORTANT: The same as in the receiver!!!
-/*//////////////////////////////////////////////////////*/
+#include "nRF24L01.h"
 
-/*Create the data struct we will send
-  The sizeof this struct should not exceed 32 bytes
-  This gives us up to 32 8 bits channals */
-RF24 radio(9, 10); // select  CSN and CE  pins
-struct MyData {
-  byte pot_value;  
-};
-MyData data;
-/*//////////////////////////////////////////////////////*/
+#include "RF24.h"
 
+int transmitterId;
 
+// Set up nRF24L01 radio on SPI bus plus pins 9 & 10
+//Contacts from the radio to connect NRF24L01 pinamnam -> Arduino
 
-//This function will only set the value to  0 if the connection is lost...
-void resetData() 
-{
-  data.pot_value = 0;  
+//SCK -> 13
+//MISO -> 12
+//MOSI -> 11
+//CSN -> 10
+//CE -> 9
+
+RF24 radio(9, 10);
+
+// this is not the channel address, but the transmitter address
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+//button connected to these pins
+
+int buttonPin1 = 2;
+
+void setup(void) {
+
+	// CHANGE THIS PER EACH TRANSMITTER, from 0 to 4
+	transmitterId = 0;
+
+	radio.begin();
+
+	// the following statements improve transmission range
+	radio.setPayloadSize(2); // setting the payload size to the needed value
+	radio.setDataRate(RF24_250KBPS); // reducing bandwidth
+
+	radio.openWritingPipe(pipe); // set the transmitter address
+
 }
 
-void setup()
-{
-  //Start everything up
-  radio.begin();
-  radio.setAutoAck(false);
-  radio.setDataRate(RF24_250KBPS);
-  radio.openWritingPipe(pipeOut);
-  resetData();
-}
+void loop(void) {
 
-/**************************************************/
+	//until the button (buttonPin1) pressed send the package (id) to receiver Arduino
+	if (digitalRead(buttonPin1) == HIGH) {
 
-// Returns a corrected value for a potentiometer analog read
-// It will map the value from 0 to 1024 to 1 to 255
-int mapPotentiometers(int val, int lower, int middle, int upper, bool reverse)
-{
-  val = constrain(val, lower, upper);
-  if ( val < middle )
-    val = map(val, lower, middle, 0, 128);
-  else
-    val = map(val, middle, upper, 128, 255);
-  return ( reverse ? 255 - val : val );
-}
+		// some implementations automatically shut down the radio after a transmission: this
+		// ensures the radio is powered up before sending data
+		radio.powerUp();
 
-void loop()
-{
-  data.pot_value = mapPotentiometers(analogRead(A2), 0, 512, 1024, true);  
-  radio.write(&data, sizeof(MyData));
+		// read and write expect a reference to the payload (& symbol)
+		// second argument is the packet length in bytes (sizeof(int) == 2)
+		radio.write(&transmitterId, 2);
+	}
 }
